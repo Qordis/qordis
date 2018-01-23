6ce30cdf610aebc11ab62a0740fbc884810c2e02
Qordis <35691978+Qordis@users.noreply.github.com>
2018-01-23 20:41:30 +0800

    1

--- bot.php
+++ bot.php
@@ -1,21 +1,20 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 echo "OK1";
-require_once("vendor/autoload.php");
 $token = "532351820:AAGl5ALv54LSQaTD-Mh5KB7gzdddTt3vRvM";
-$bot = new \TelegramBot\Api\BotApi($token);
-$bot->command('start', function ($message) use ($bot) {
-    $answer = 'Добро пожаловать!';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
+require_once "vendor/autoload.php";
+
+try {
+    $bot = new \TelegramBot\Api\Client($token);
+    $bot->command('ping', function ($message) use ($bot) {
+    $bot->sendMessage($message->getChat()->getId(), 'pong!');
+    });
+    
+    $bot->run();
+
+} catch (\TelegramBot\Api\Exception $e) {
+    $e->getMessage();
+}
+
 echo "OK2";
-$bot->command('help', function ($message) use ($bot) {
-    $answer = 'Команды:
-/help - помощ';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
-
-$bot->run();
-
-echo "OK3";
 ?>
