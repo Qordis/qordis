dd7c04dc8cf8c525b8999c68cc63440e727dc195
Qordis <35691978+Qordis@users.noreply.github.com>
2018-01-23 20:20:34 +0800

    4

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-echo "123"
+header('Content-Type: text/html; charset=utf-8');
+require_once("vendor/autoload.php");
+$token = "532351820:AAGl5ALv54LSQaTD-Mh5KB7gzdddTt3vRvM";
+$bot = new \TelegramBot\Api\Client($token);
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - помощ';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+$bot->run();
 ?>
